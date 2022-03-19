b51ceed0a265831b1c5d731c7b3f510e9536bce0
salma45667 <salmamalak555555@gmail.com>
2022-03-19 20:58:57 +0300

    Cw6 3

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,28 @@
 // هنا سوف تنشئ دالة calculate 
+function calculate(){
+    let height = document.getElementById("height").value;
+    let weight = document.getElementById("weight").value;
+
+
+//Error Message (Alert)
+if (height =="" || weight =="") {
+  alert("قم بالتأكد من تعبئة جميع البيانات المطلوبة!");
+  return; 
+}
+
+let BMI = weight / (height * height);
+let status ="";
+
+if (BMI < 18.5) {
+    status = "انت تعانى من الوزن الزائد!"
+} else if (BMI < 25 ) {
+    status = "وزنك صحى";
+} else if (BMI < 30) {
+    status = "تعانى من زيادة فى الوزن";
+} else {
+    status = "تعانى من السمنة المفرطة";
+}
+
+BMI = BMI.toFixed(2); 
+document.getElementById("result").innerHTML = BMI;
+document.getElementsByClassName("comment")[0].innerHTML = status;
